286ddaf115855fa947835a73a1240e6a90d7869b
Sania Singla <singlasania7717@gmail.com>
2024-11-23 22:37:53 +0530

    configured CORS

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,7 +1,26 @@
-import cookieParser from "cookie-parser";
-import express from "express";
+import cookieParser from 'cookie-parser';
+import express from 'express';
+import cors from 'cors';
 export const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+// Configure CORS
+const whitelist = process.env.WHITELIST ? process.env.WHITELIST.split(',') : [];
+app.use(
+    cors({
+        origin: function (origin, callback) {
+            if (!origin || whitelist.includes(origin)) {
+                callback(null, true);
+            } else {
+                callback(new Error('Not allowed by CORS'));
+            }
+        },
+        methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+        credentials: true,
+        optionsSuccessStatus: 200,
+        allowedHeaders: ['Content-Type', 'Authorization'],
+    })
+);
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,5 @@
 import './config/envLoader.js';
-import { app } from "./app.js";
+import { app } from './app.js';
 
 const PORT = process.env.PORT || 4000;
 
