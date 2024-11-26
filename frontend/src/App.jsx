87c3ad49b3d8edbc0321a1366cb0ba16c8ccc143
karan20991 <kp647297@gmail.com>
2024-11-26 10:54:41 +0530

    test

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,9 @@
 function App() {
     return (
         <div className="w-screen h-screen content-center text-center">
-            <p className="text-5xl font-bold">Ministry of ayush - registeration portal jatin</p>
+            <p className="text-5xl font-bold">
+                Ministry of ayush - registeration portal jatin
+            </p>
         </div>
     );
 }
