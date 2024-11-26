7b114d60f41b47fc29b2bf97335848529d973991
karan20991 <kp647297@gmail.com>
2024-11-26 11:10:05 +0530

    new

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ function App() {
     return (
         <div className="w-screen h-screen content-center text-center">
             <p className="text-5xl font-bold">
-                Ministry of ayush - registeration 
+                Ministry of ayush
             </p>
         </div>
     );
