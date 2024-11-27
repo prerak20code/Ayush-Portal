7019b01ff2a93cc1533dbe066e18954aa887006d
Vatshal Sanghai <=>
2024-11-27 15:02:54 +0530

    Checking

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ function App() {
     return (
         <div className="w-screen h-screen content-center text-center">
             <p className="text-5xl font-bold">
-                Ministry of ayush
+                Ministry of ayush - simplifying startup registration 
             </p>
         </div>
     );
