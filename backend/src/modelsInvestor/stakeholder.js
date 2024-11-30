9e938edf4745baaabba9d7554819c27a032d167e
preraknagpal <prerak20coder@gmail.com>
2024-12-01 02:46:25 +0530

    Changes to be committed: 	modified:   backend/package-lock.json 	modified:   backend/package.json 	modified:   backend/src/modelsInvestor/stakeholder.js 	modified:   backend/src/routes/stakeholder.js 	modified:   frontend/package-lock.json 	modified:   frontend/package.json 	modified:   package-lock.json 	modified:   package.json

--- backend/src/modelsInvestor/stakeholder.js
+++ backend/src/modelsInvestor/stakeholder.js
@@ -1,49 +1,48 @@
-import mongoose from "mongoose";
-import Schema from mongoose.Schema;
-import passportLocalMongoose from "passport-local-mongoose";
-import { application } from "express";
+import mongoose from 'mongoose';
+import passportLocalMongoose from 'passport-local-mongoose';
+import { application } from 'express';
 
 const InvestorSchema = new Schema({
-    Name:{
-        type:String,
+    Name: {
+        type: String,
         required: true,
     },
-    organisationName:{
-        type:String,
+    organisationName: {
+        type: String,
         required: true,
     },
-    Interest:{
-        type:String,
+    Interest: {
+        type: String,
         required: true,
     },
-   PhoneNumber:{
-        type:Number,
-        required: true,
-    },
-
-    email:{
-        type : String,
+    PhoneNumber: {
+        type: Number,
         required: true,
     },
 
-    connectedStartups:[
+    email: {
+        type: String,
+        required: true,
+    },
+
+    connectedStartups: [
         {
             type: Schema.Types.ObjectId,
-            ref: "Startups",
-        }
+            ref: 'Startups',
+        },
     ],
-    likedStartups:[
+    likedStartups: [
         {
             type: Schema.Types.ObjectId,
-            ref: "Startups",
-        }
+            ref: 'Startups',
+        },
     ],
-    applicationStatus:[
+    applicationStatus: [
         {
             type: Schema.Types.ObjectId,
-            ref: "notification",
-        }
-    ]
+            ref: 'notification',
+        },
+    ],
 });
 
 InvestorSchema.plugin(passportLocalMongoose);
