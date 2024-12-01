7bb77cd16fdafd69c1a8480f447b7a629a1ec0ed
Vatshal Sanghai <=>
2024-12-01 20:02:07 +0530

    error resolved

--- backend/src/modelsInvestor/stakeholder.js
+++ backend/src/modelsInvestor/stakeholder.js
@@ -3,66 +3,56 @@ import passportLocalMongoose from 'passport-local-mongoose';
 
 const { Schema } = mongoose;
 const InvestorSchema = new Schema({
-    Name: {
-        type: String,
+  
     Name: {
         type: String,
         required: true,
     },
-    organisationName: {
-        type: String,
+   
     organisationName: {
         type: String,
         required: true,
     },
-    Interest: {
-        type: String,
+  
     Interest: {
         type: String,
         required: true,
     },
-    PhoneNumber: {
-        type: Number,
+  
     PhoneNumber: {
         type: Number,
         required: true,
     },
 
-    email: {
-        type: String,
+ 
     email: {
         type: String,
         required: true,
     },
 
-    connectedStartups: [
+  
     connectedStartups: [
         {
             type: Schema.Types.ObjectId,
             ref: 'Startups',
         },
-            ref: 'Startups',
-        },
+      
     ],
-    likedStartups: [
+   
     likedStartups: [
         {
             type: Schema.Types.ObjectId,
             ref: 'Startups',
         },
-            ref: 'Startups',
-        },
+           
     ],
-    applicationStatus: [
+    
     applicationStatus: [
         {
             type: Schema.Types.ObjectId,
             ref: 'notification',
         },
-    ],
-            ref: 'notification',
-        },
-    ],
+    ]
 });
 
 InvestorSchema.plugin(passportLocalMongoose);
