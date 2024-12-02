59311d022209148934c8b1b1eb2fe1e08228e972
Sania Singla <singlasania7717@gmail.com>
2024-12-02 13:53:29 +0530

    resolved import errors

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,7 @@
 import express from 'express';
 export const app = express();
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
--- backend/src/routes/user.Router.js
+++ backend/src/routes/user.Router.js
@@ -3,18 +3,18 @@ export const userRouter = express.Router();
 import {
     register,
     login,
-    resetPassword,
-    requestResetPassword,
-    sendPasswordResetEmail,
+    // resetPassword,
+    // requestResetPassword,
+    // sendPasswordResetEmail,
 } from '../controllers/user.Controller.js';
 
 import { verifyJWT } from '../middlewares/index.js';
 
 userRouter.post('/register', register);
 userRouter.post('/login', login);
-userRouter.post(
-    '/reset-password',
-    verifyJWT,
-    requestResetPassword,
-    resetPassword
-);
+// userRouter.post(
+//     '/reset-password',
+//     verifyJWT,
+//     requestResetPassword,
+//     resetPassword
+// );
