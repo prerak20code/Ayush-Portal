1232c0b97b87207e9d5714e573fb0d280e0a03c7
Sania Singla <singlasania7717@gmail.com>
2024-12-11 20:13:47 +0530

    jkb

--- frontend/src/components/layout/Header.jsx
+++ frontend/src/components/layout/Header.jsx
@@ -224,6 +224,7 @@ export default function Header() {
             {/* tabs header */}
             <div className="relative drop-shadow-md bg-[#f68533] flex items-center justify-between h-[40px] gap-x-8 px-4">
                 <img
+                    onClick={() => navigate('/')}
                     src={AYUSHSTARTUPLOGO}
                     alt="gov india image"
                     className="object-contain h-full"
